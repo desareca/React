14ee8da3165f4c30fcec67de6b3d52c609e09ceb
desareca <carlos.saquel@gmail.com>
2021-07-31 15:13:01 -0400

    hello dojo - v1

---
+++ hello_dojo/hello.js
@@ -0,0 +1,12 @@
+const content = React.createElement('div', null,
+    React.createElement('h1', null, 'Hello Dojo!'),
+    React.createElement('h2', null, 'Things I need to do:'),
+    React.createElement('ul', null,
+        React.createElement('li', null, 'Learn React'),
+        React.createElement('li', null, 'Climb Mt. Everest'),
+        React.createElement('li', null, 'Run a marathon'),
+        React.createElement('li', null, 'Feed the dogs'),
+    )
+);
+
+ReactDOM.render(content, document.getElementById('hello'));
